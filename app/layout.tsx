40c1223954fbdfc5267cdd819b05a169f46fc073
volant97 <volant97@naver.com>
2024-08-21 18:34:28 +0900

    [CSS] 0.0.4 layout 크기 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} max-w-[360px] mx-auto bg-neutral-900 text-white h-screen border-2`}
+        className={`${inter.className} max-w-md mx-auto bg-neutral-900 text-white h-[100dvh] border-2`}
       >
         {children}
       </body>
     </html>
   );
 }
+
+// crosswalk
+// h-[100dvh] w-full max-w-[430px] min-w-[360px] border-x overflow-y-auto overflow-x-hidden scrollbar-hide
