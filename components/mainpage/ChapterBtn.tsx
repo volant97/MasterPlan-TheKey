5e4f5c40aef3eb6d52371740f9209eead742ec8d
volant97 <volant97@naver.com>
2024-08-25 22:10:02 +0900

    [UPDATE] 0.1.4 Main Page - 챕터 버튼 hover scale 설정

--- components/mainpage/ChapterBtn.tsx
+++ components/mainpage/ChapterBtn.tsx
@@ -9,7 +9,7 @@ export default function ChapterBtn({ chapter, isActivated }: Props) {
   return (
     <Link href={`/chapter/${chapter}`}>
       <button
-        className="flex flex-col justify-center items-center size-24 rounded-2xl border-4 border-yellow-600 bg-yellow-950 shadow-md shadow-black *:font-SDSamliphopangche cursor-pointer enabled:active:scale-95 enabled:active:bg-yellow-600 disabled:opacity-50 disabled:cursor-default "
+        className="flex flex-col justify-center items-center size-24 rounded-2xl border-4 border-yellow-600 bg-yellow-950 shadow-md shadow-black *:font-SDSamliphopangche cursor-pointer enabled:active:scale-95 enabled:active:bg-yellow-600 enabled:hover:scale-105 disabled:opacity-50 disabled:cursor-default"
         disabled={!isActivated}
       >
         <p className="text-2xl">{chapter}</p>
