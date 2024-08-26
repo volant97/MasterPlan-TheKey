bf09dd62a0551e7b9a2f8d98e95aa82d498e4a8e
volant97 <volant97@naver.com>
2024-08-26 23:34:58 +0900

    [UPDATE] 0.3.9 진술 파트 컴포넌트화

--- app/chapter/[id]/page.tsx
+++ app/chapter/[id]/page.tsx
@@ -9,6 +9,7 @@ import SuspectPart from "@/components/chapter/SuspectPart";
 import PlacePart from "@/components/chapter/PlacePart";
 import WeaponPart from "@/components/chapter/WeaponPart";
 import CluePart from "@/components/chapter/CluePart";
+import StatementPart from "@/components/chapter/StatementPart";
 
 interface Props {
   params: { id: string };
@@ -56,43 +57,7 @@ export default function ChapterPage({ params }: Props) {
       <CluePart data={data.clues} />
 
       {/* 6 - 진술 */}
-      <div className="flex flex-col w-full">
-        <div className="relative w-full h-10">
-          <h2 className="absolute left-1 -rotate-12 text-xl bg-orange-400 rounded-lg px-2 py-1">
-            진술
-          </h2>
-          <p className="absolute right-0 bottom-3 text-xs text-orange-400">
-            범인은 거짓말을 합니다.
-          </p>
-          <span className="absolute left-[5%] top-8 w-[95%] border-b-2 border-orange-400" />
-        </div>
-        <div>
-          <div className="flex flex-col gap-2 px-2 py-4">
-            <div className="flex gap-2">
-              <span>▶</span>
-              <div>
-                <p className="text-orange-400">레이디 바이올렛</p>
-                <p>평민은 믿을 게 못 되죠.</p>
-                <p>버밀리온 공작부인이 빗자루를 갖고 왔어요.</p>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <span>▶</span>
-              <div>
-                <p className="text-orange-400">모브 부사장</p>
-                <p>난 빗자루를 가져오지 않았는데.</p>
-              </div>
-            </div>
-            <div className="flex gap-2">
-              <span>▶</span>
-              <div>
-                <p className="text-orange-400">버밀리온 공작부인</p>
-                <p>나는 나무토막을 가져오지 않았어.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <StatementPart data={data.statements} />
 
       {/* 7 - 정답 */}
       <div className="flex flex-col w-full mb-6">
---
+++ components/chapter/StatementPart.tsx
@@ -0,0 +1,36 @@
+import { Statement } from "@/types/chapterJsonData";
+
+interface Props {
+  data: Statement[];
+}
+
+export default function StatementPart({ data }: Props) {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="relative w-full h-10">
+        <h2 className="absolute left-1 -rotate-12 text-xl bg-orange-400 rounded-lg px-2 py-1">
+          진술
+        </h2>
+        <p className="absolute right-0 bottom-3 text-xs text-orange-400">
+          범인은 거짓말을 합니다.
+        </p>
+        <span className="absolute left-[5%] top-8 w-[95%] border-b-2 border-orange-400" />
+      </div>
+      <div>
+        <div className="flex flex-col gap-2 px-2 py-4">
+          {data.map((v, i) => (
+            <div key={i} className="flex gap-2">
+              <span>▶</span>
+              <div>
+                <p className="text-orange-400">{v.person}</p>
+                {v.text.map((v, i) => (
+                  <p key={i}>{v}</p>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
