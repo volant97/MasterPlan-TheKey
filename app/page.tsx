8105093fef17356968dffc8ff8030f81a47e5c53
volant97 <volant97@naver.com>
2024-09-05 20:38:17 +0900

    [UPDATE] 0.5.1 사건2 제작중

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function MainPage() {
     <div className="flex justify-center items-center">
       <div className="grid grid-cols-3 grid-flow-row gap-4">
         <ChapterBtn chapter="1" isActivated={true} />
-        <ChapterBtn chapter="2" isActivated={false} />
+        <ChapterBtn chapter="2" isActivated={true} />
         <ChapterBtn chapter="3" isActivated={false} />
         <ChapterBtn chapter="4" isActivated={false} />
         <ChapterBtn chapter="5" isActivated={false} />
